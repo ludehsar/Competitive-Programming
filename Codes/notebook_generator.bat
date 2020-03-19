6a3d7a665c69f46e90275be41876c02b2df67229
Rashedul Alam <rashedulalam112112@gmail.com>
2020-03-20 02:23:08 +0600

    Changed the batch file a little bit

--- Codes/notebook_generator.bat
+++ Codes/notebook_generator.bat
@@ -4,5 +4,5 @@ where notebook-generator > nul
 if %ERRORLEVEL% GEQ 1 (npm install -g notebook-generator) else (echo Notebook generator is already installed, proceeding further)
 set /p team="Enter your team name: "
 set /p university="Enter your university name: "
-call notebook-generator ./ --author "%team% - %university%" --initials "%university% (%team%)" --paper a4paper
+call notebook-generator ./ --author "%team% - %university%" --initials "%university% (%team%)" --size 8 --columns 3 --paper a4paper
 pause
