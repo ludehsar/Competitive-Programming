e83d110a5e7f67d070798f4d651eceecf72ae8ab
Rashedul Alam <rashedulalam112112@gmail.com>
2020-03-27 19:55:07 +0600

    Modified sparse table

--- Codes/CU BadToTheBone/Data Structures/sparse_table.cpp
+++ Codes/CU BadToTheBone/Data Structures/sparse_table.cpp
@@ -1,15 +1,11 @@
-int log[MAXN+1];
 int st[MAXN][K];
 
 void preprocess()
 {
-    log[1] = 0;
-    for (int i = 2; i <= MAXN; i++) log[i] = log[i/2] + 1;
-
-    for (int i = 0; i < N; i++) st[i][0] = array[i];
+    for (int i = 1; i <= N; i++) st[i][0] = array[i];
 
     for (int j = 1; j <= K; j++) {
-        for (int i = 0; i + (1 << j) <= N; i++) {
+        for (int i = 1; i + (1 << j) <= N; i++) {
             st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
         }
     }
@@ -17,6 +13,6 @@ void preprocess()
 
 int getMinimum(int L, int R)
 {
-    int j = log[R - L + 1];
+    int j = log2(R - L + 1);
     return min(st[L][j], st[R - (1 << j) + 1][j]);
 }
